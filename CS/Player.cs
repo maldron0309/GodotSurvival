d939a93ffbfe0e422dbfe5a5654ea4b0be71cd3b
maldron0309 <bagjinhyeong640@gmail.com>
2024-01-01 23:23:56 +0900

    [add] Player walk, idle animation

---
+++ CS/Player.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	[Export] float speed { get; set; } = 400;
+
+	public override void _Ready()
+	{
+
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Move();
+		MoveAndSlide();
+	}
+
+	/// <summary>
+	/// Player Move 
+	/// </summary>
+	private void Move()
+	{
+		Vector2 dir = Input.GetVector("Left", "Right", "Up", "Down");
+		Velocity = dir * speed;
+
+		Node happyBoo = GetNode("HappyBoo");
+
+		if (Velocity.Length() > 0)
+		{
+			happyBoo.Call("play_walk_animation");
+		}else{
+			happyBoo.Call("play_idle_animation");
+		}
+	}
+}
