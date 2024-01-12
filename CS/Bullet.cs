2b47a84376c64f16a9c912fc038b4862e40aab63
maldron0309 <bagjinhyeong640@gmail.com>
2024-01-13 05:03:56 +0900

    [ add ] bullet 중간저장

---
+++ CS/Bullet.cs
@@ -0,0 +1,53 @@
+using Godot;
+using System;
+
+public interface IDamageable
+{
+    void TakeDamage();
+}
+
+
+public partial class Damageable : Area2D, IDamageable
+{
+    public void TakeDamage()
+    {
+        GD.Print("Damage taken");
+    }
+}
+
+public partial class Bullet : Area2D
+{
+
+    private float travelledDistance = 0;
+    private const float RANGE = 1200;
+    private const float SPEED = 1000;
+
+    public override void _Ready()
+    {
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+
+        var dir = Vector2.Right.Rotated(Rotation);
+        Position += dir * SPEED * (float)delta;
+
+        travelledDistance += SPEED * (float)delta;
+
+        if (travelledDistance > RANGE)
+        {
+            QueueFree();
+        }
+    }
+
+    public void _on_Body_entered(Node body)
+    {
+        QueueFree();
+
+        if (body.HasMethod("TakeDamage"))
+        {
+            body.Call("TakeDamage");
+        }
+    }
+}
--- CS/Gun.cs
+++ CS/Gun.cs
@@ -3,20 +3,22 @@ using System;
 
 public partial class Gun : Area2D
 {
-	public override void _Ready()
-	{ 
-		
-	}
 
 	public override void _Process(double delta)
 	{
 		var enemiesInRange = GetOverlappingBodies();
-		
+
 		if (enemiesInRange.Count > 0)
 		{
 			var targetEnemy = enemiesInRange[0];
 			LookAt(targetEnemy.GlobalPosition);
 		}
+	}
+
+	public void Shoot()
+	{
+		var BULLET = ResourceLoader.Load<PackedScene>("res://bullet.tscn");
+
 	}
 }
 
